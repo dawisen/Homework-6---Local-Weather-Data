d6b5ad7ef1d84d1b3c48630d6cd6357f4e3b88e8
gudetama <daniellewwise@gmail.com>
2020-10-21 00:21:37 -0400

    updating api, js, css

--- script.js
+++ script.js
@@ -1,22 +1,110 @@
 // api key = a1f89a9256d059b7eb136f9da385a577
-// API call url = api.openweathermap.org/data/2.5/weather?q={ city name },{ state code }&appid=a1f89a9256d059b7eb136f9da385a577;
-
-
-// button
-$("#search").on("click", function(event) {
+// API call url = api.openweathermap.org/data/2.5/weather?q={ city name }&appid=a1f89a9256d059b7eb136f9da385a577;
+$(document).ready(function () {
+  loadDefaultCity();
+  // search typing city name button
+  $("#search").on("click", function (event) {
     event.preventDefault();
     // Here we grab the text from the input box
     var cityName = $("#city-name").val();
     // Here we construct our URL
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=a1f89a9256d059b7eb136f9da385a577";
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).then(function (response) {
-          console.log(response)
-          $(".weather-data").text(JSON.stringify(response));
-        });
-})
+    var queryURL =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      cityName +
+      "&appid=a1f89a9256d059b7eb136f9da385a577";
+    
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+      console.log("icon code = " + response.weather[0].icon);
+
+      var iconUrl =
+        "https://openweathermap.org/img/wn/" +
+        response.weather[0].icon +
+        "@2x.png";
+      console.log("var iconUrl: " + iconUrl);
+      $("h2.cityName").html(response.name + " " + "(" + moment().format("L") + ")");
+      $("#weatherIcon").attr("src", iconUrl);
+      $("p.temp").text(
+        "Temperature: " +
+          Math.floor((response.main.temp - 273.15) * 1.8 + 32) +
+          "°"
+      );
+      $("p.humidity").text("Humidity: " + response.main.humidity + "%");
+      $("p.wind").text("Wind Speed: " + response.wind.speed + " " + "MPH");
+      $("p.UVindex").text("UV Index: ");
+    });
+  });
+});
+
+$(".list-group-item").on("click", function (event) {
+  event.preventDefault();
+  var cityName = $(this).text();
+  console.log(cityName);
+  var queryURL =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      cityName +
+      "&appid=a1f89a9256d059b7eb136f9da385a577";
+    
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+      console.log("icon code = " + response.weather[0].icon);
+
+      var iconUrl =
+        "https://openweathermap.org/img/wn/" +
+        response.weather[0].icon +
+        "@2x.png";
+      console.log("var iconUrl: " + iconUrl);
+      $("h2.cityName").html(response.name + " " + "(" + moment().format("L") + ")");
+      $("#weatherIcon").attr("src", iconUrl);
+      $("p.temp").text(
+        "Temperature: " +
+          Math.floor((response.main.temp - 273.15) * 1.8 + 32) +
+          "°"
+      );
+      $("p.humidity").text("Humidity: " + response.main.humidity + "%");
+      $("p.wind").text("Wind Speed: " + response.wind.speed + " " + "MPH");
+      $("p.UVindex").text("UV Index: ");
+    });
+});
 
 
-//******* add UV index button that changes color or certain index levels (green to red)67767688
+function loadDefaultCity() {
+   var queryURL =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      "raleigh" +
+      "&appid=a1f89a9256d059b7eb136f9da385a577";
+    
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+      console.log("icon code = " + response.weather[0].icon);
+
+      var iconUrl =
+        "https://openweathermap.org/img/wn/" +
+        response.weather[0].icon +
+        "@2x.png";
+      console.log("var iconUrl: " + iconUrl);
+      $("h2.cityName").html(response.name + " " + "(" + moment().format("L") + ")");
+      $("#weatherIcon").attr("src", iconUrl);
+      $("p.temp").text(
+        "Temperature: " +
+          Math.floor((response.main.temp - 273.15) * 1.8 + 32) +
+          "°"
+      );
+      $("p.humidity").text("Humidity: " + response.main.humidity + "%");
+      $("p.wind").text("Wind Speed: " + response.wind.speed + " " + "MPH");
+      $("p.UVindex").text("UV Index: ");
+    });
+}
+
+//******* add UV index button that changes color or certain index levels (green to red)
+// http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid={API key} <-- UV index API
+// make a call using the first ajax calls response.coord.lat and response.coord.lon
